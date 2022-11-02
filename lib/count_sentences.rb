54e2bc880ab1009f7cb84c81bee9f209a0c1bfa0
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-02 11:43:59 -0600

    Completed lab.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self[-1] == "."
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    my_split = self.split(/[!?.]/).filter do |string|
+      string != ""
+    end
+    my_split.length
   end
 end
